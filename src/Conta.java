859cdb35cb1a3281f167c8297e4ea7a65346094d
AnaCarol21 <carol.agl@live.com>
2024-06-07 19:36:49 -0300

    add: alteracoes e finalizacoes do exemplo

--- src/Conta.java
+++ src/Conta.java
@@ -10,16 +10,17 @@ public abstract class Conta implements IConta {
     public Conta(){
         this.agencia = AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
+        this.saldo = 0.0;
     }
 
     @Override
     public void sacar(double valor) {
-        this.saldo -= valor;        
+        saldo -= valor;        
     }
 
     @Override
     public void depositar(double valor) {
-        this.saldo += valor;                
+        saldo += valor;                
     }
 
     @Override
@@ -43,6 +44,6 @@ public abstract class Conta implements IConta {
     protected void imprimirInfoComuns(){
         System.out.println(String.format("Agencia %d",this.agencia));
         System.out.println(String.format("Numero %d",this.numero));
-        System.out.println(String.format("Saldo %d",this.saldo));
+        System.out.println(String.format("Saldo %.2f",this.saldo));
     }
 }
